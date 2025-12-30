c35f396432f29a0210357efb1d4e009185574d03
Zhuo Chen <zxc417@student.bham.ac.uk>
2025-12-30 13:12:23 +0000

    misc: Rename the attribute model to ae for better readability.

--- scripts/train_gridae.py
+++ scripts/train_gridae.py
@@ -27,16 +27,16 @@ def main(cfg):
         logger = TensorBoardLogger(save_dir='logs/tb_logs')
     else:
         import wandb
-        logger = WandbLogger(name=cfg.model.name+'-'+t.strftime('%Y%m%d-%H%M%S'),
+        logger = WandbLogger(name=cfg.ae.name+'-'+t.strftime('%Y%m%d-%H%M%S'),
                                     project='LG3DContact',
                                     log_model=True, save_dir='logs/wandb_logs')
-    
+
     # Initialize the model, data module, and trainer
-    model_name = cfg.model.name.upper()
+    model_name = cfg.ae.name.upper()
     model_module = importlib.import_module(f"common.model.{model_name.lower()}.{model_name.lower()}")
     model_class = getattr(model_module, model_name)
-    model = model_class(cfg.model)
-    # input = torch.randn(3, cfg.model.in_dim, 8, 8, 8)
+    model = model_class(cfg.ae)
+    # input = torch.randn(3, cfg.ae.in_dim, 8, 8, 8)
     # embedding_loss, recon, perplexity = model(input, verbose=True)
     data_module = LocalGridDataModule(cfg)
     if cfg.run_phase == 'train':
