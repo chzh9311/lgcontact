fix: Update hand_translation calculation in HandObject.

common/model/diff/unet.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 from omegaconf import DictConfig
 
 from .utils import timestep_embedding
-from .utils import ResBlock, SpatialTransformer
+from .utils import ResBlock, SpatialTransformer, CrossSpatialTransformer
 from common.model.pointnet_module import Pointnet_feat_net
 # from .scene_model import create_scene_model
 
@@ -136,14 +136,47 @@ class UNetModel(nn.Module):
         return obj_feat
 
 
-class GraspUNetModel(UNetModel):
+class DualUNetModel(nn.Module):
+    """
+    Dual diffusion UNet model that separately processes local and global features, with cross attention to fuse them.
+    """
     def __init__(self, cfg: DictConfig) -> None:
-        super(GraspUNetModel, self).__init__(cfg)
-        self.layers = nn.ModuleList()
+        super(DualUNetModel, self).__init__()
+
+        self.d_x = cfg.d_x
+        self.d_model = cfg.d_model
+        self.nblocks = cfg.nblocks
+        self.resblock_dropout = cfg.resblock_dropout
+        self.transformer_num_heads = cfg.transformer_num_heads
+        self.transformer_dim_head = cfg.transformer_dim_head
+        self.transformer_dropout = cfg.transformer_dropout
+        self.transformer_depth = cfg.transformer_depth
+        self.transformer_mult_ff = cfg.transformer_mult_ff
+        self.context_dim = cfg.context_dim
+        self.obj_feat_dim = cfg.obj_feat_dim
+        self.use_position_embedding = cfg.use_position_embedding # for input sequence x
+
+        ## create scene model from config
+        # self.scene_model = create_scene_model(cfg.scene_model.name, **scene_model_args)
+        self.obj_feat_net = Pointnet_feat_net(**cfg.obj_encoder)
+
+        time_embed_dim = self.d_model * cfg.time_embed_mult
+        self.time_embed = nn.Sequential(
+            nn.Linear(self.d_model, time_embed_dim),
+            nn.SiLU(),
+            nn.Linear(time_embed_dim, time_embed_dim),
+        )
+        
+        self.in_layers = nn.Sequential(
+            nn.Conv1d(self.d_x, self.d_model, 1)
+        )
+        self.local_layers = nn.ModuleList()
+        self.global_layers = nn.ModuleList()
+        self.cross_layers = nn.ModuleList()
         time_embed_dim = self.d_model * cfg.time_embed_mult
 
         for i in range(self.nblocks):
-            self.layers.append(
+            self.local_layers.append(
                 ResBlock(
                     self.d_model,
                     time_embed_dim,
@@ -152,9 +185,18 @@ class GraspUNetModel(UNetModel):
                     self.d_model,
                 )
             )
+            self.global_layers.append(
+                ResBlock(
+                    self.d_model,
+                    time_embed_dim,
+                    0, # no object feature here
+                    self.resblock_dropout,
+                    self.d_model,
+                )
+            )
             ## With cross Attention
-            self.layers.append(
-                SpatialTransformer(
+            self.cross_layers.append(
+                CrossSpatialTransformer(
                     self.d_model, 
                     self.transformer_num_heads, 
                     self.transformer_dim_head, 
@@ -164,3 +206,70 @@ class GraspUNetModel(UNetModel):
                     context_dim=self.context_dim,
                 )
             )
+
+        self.local_out_layers = nn.Sequential(
+            nn.GroupNorm(32, self.d_model),
+            nn.SiLU(),
+            nn.Conv1d(self.d_model, self.d_x, 1),
+        )
+
+        self.global_out_layers = nn.Sequential(
+            nn.GroupNorm(32, self.d_model),
+            nn.SiLU(),
+            nn.Conv1d(self.d_model, self.d_y, 1),
+        )
+
+
+    def forward(self, x_t: torch.Tensor, y_t: torch.Tensor, ts: torch.Tensor, cond: torch.Tensor) -> torch.Tensor:
+        """ Apply the model to an input batch
+
+        Args:
+            x_t: the input local data, <B, L, C>
+            y_t: the input global data, <B, C>
+            ts: timestep, 1-D batch of timesteps
+            cond: condition feature
+        
+        Return:
+            the denoised target data, i.e., $x_{t-1}$
+        """
+        x_in_shape = len(x_t.shape)
+        if x_in_shape == 2:
+            x_t = x_t.unsqueeze(1)
+        assert len(x_t.shape) == 3
+
+        y_in_shape = len(y_t.shape)
+        if y_in_shape == 2:
+            y_t = y_t.unsqueeze(1)
+        assert len(y_t.shape) == 3
+
+        ## time embedding
+        t_emb = timestep_embedding(ts, self.d_model)
+        t_emb = self.time_embed(t_emb)
+
+        h = rearrange(x_t, 'b l c -> b c l')
+        h = self.in_layers(h) # <B, d_model, L>
+        # print(h.shape, cond.shape) # <B, d_model, L>, <B, T , c_dim>
+
+        ## prepare position embedding for input x
+        if self.use_position_embedding:
+            B, DX, TX = h.shape
+            pos_Q = torch.arange(TX, dtype=h.dtype, device=h.device)
+            pos_embedding_Q = timestep_embedding(pos_Q, DX) # <L, d_model>
+            h = h + pos_embedding_Q.permute(1, 0) # <B, d_model, L>
+
+        for i in range(self.nblocks):
+            h_local = self.local_layers[i](h, t_emb, cond)
+            h_global = self.global_layers[i](h, t_emb)
+            h = self.cross_layers[i](h_global, h_local)
+        h_local = self.local_out_layers(h)
+        h_global = self.global_out_layers(h_global)
+        h_local = rearrange(h_local, 'b c l -> b l c')
+        h_global = rearrange(h_global, 'b c l -> b l c')
+
+        ## reverse to original shape
+        if x_in_shape == 2:
+            h_local = h_local.squeeze(1)
+
+        if y_in_shape == 2:
+            h_global = h_global.squeeze(1)
+        return h_local, h_global

common/model/diff/utils.py
@@ -64,10 +64,11 @@ class ResBlock(nn.Module):
             nn.Linear(self.emb_channels, self.out_channels)
         )
         
-        self.obj_emb_layers = nn.Sequential(
-            nn.SiLU(),
-            nn.Conv1d(self.obj_feat_dim, self.out_channels, 1)
-        )
+        if self.obj_feat_dim > 0:
+            self.obj_emb_layers = nn.Sequential(
+                nn.SiLU(),
+                nn.Conv1d(self.obj_feat_dim, self.out_channels, 1)
+            )
 
         self.out_layers = nn.Sequential(
             nn.GroupNorm(32, self.out_channels),
@@ -81,7 +82,7 @@ class ResBlock(nn.Module):
         else:
             self.skip_connection = nn.Conv1d(self.in_channels, self.out_channels, 1)
 
-    def forward(self, x, emb, obj_emb):
+    def forward(self, x, emb, obj_emb=None):
         """
         Apply the block to a Tensor, conditioned on a timestep embedding.
         :param x: an [N x C x ...] Tensor of features.
@@ -89,12 +90,15 @@ class ResBlock(nn.Module):
         :param obj_emb: an [N x obj_feat_dim*2] Tensor of object embeddings.
         :return: an [N x C x ...] Tensor of outputs.
         """
-        h = self.in_layers(x) + self.obj_emb_layers(obj_emb)
+        if self.obj_feat_dim > 0:
+            obj_feat = self.obj_emb_layers(obj_emb)
+        else:
+            obj_feat = 0
+        h = self.in_layers(x) + obj_feat
         emb_out = self.emb_layers(emb)
         h = h + emb_out.unsqueeze(-1)
         h = self.out_layers(h)
         return self.skip_connection(x) + h
-
 
 
 def exists(val):
@@ -279,6 +283,24 @@ class BasicTransformerBlock(nn.Module):
         x = self.ff(self.norm2(x)) + x
         return x
 
+
+class CrossTransformerBlock(nn.Module):
+    def __init__(self, dim, n_heads, d_head, dropout=0., context_dim=None, gated_ff=True, mult_ff=2):
+        super().__init__()
+        self.attn1 = CrossAttention(query_dim=dim, heads=n_heads, dim_head=d_head, dropout=dropout)  # is a self-attention
+        self.ff = FeedForward(dim, dropout=dropout, glu=gated_ff, mult=mult_ff)
+        self.attn2 = CrossAttention(query_dim=dim, context_dim=context_dim,
+                                    heads=n_heads, dim_head=d_head, dropout=dropout)  # is self-attn if context is none
+        self.norm1 = nn.LayerNorm(dim)
+        self.norm2 = nn.LayerNorm(dim)
+        self.norm3 = nn.LayerNorm(dim)
+
+    def forward(self, x, context=None):
+        x = self.attn1(self.norm1(x)) + x
+        x = self.attn2(self.norm2(x), context=context) + x
+        x = self.ff(self.norm3(x)) + x
+        return x
+
 class SpatialTransformer(nn.Module):
     """
     Transformer block for sequential data.
@@ -323,6 +345,18 @@ class SpatialTransformer(nn.Module):
         x = rearrange(x, 'b l c -> b c l')
         x = self.proj_out(x)
         return x + x_in
+
+
+class CrossSpatialTransformer(SpatialTransformer):
+    def __init__(self, in_channels, n_heads=8, d_head=64,
+                 depth=1, dropout=0., context_dim=None, mult_ff=2):
+        super().__init__(in_channels, n_heads, d_head, depth, dropout, context_dim, mult_ff)
+        inner_dim = n_heads * d_head
+        self.transformer_blocks = nn.ModuleList(
+            [CrossTransformerBlock(inner_dim, n_heads, d_head, dropout=dropout, context_dim=context_dim, mult_ff=mult_ff)
+                for d in range(depth)]
+        )
+
 
 if __name__ == '__main__':
     st = SpatialTransformer(256, 8, 64, 6, context_dim=768)

common/model/graspdifftrainer.py
@@ -21,8 +21,9 @@ class GraspDiffTrainer(LGCDiffTrainer):
     """
     The Lightning trainer interface to train Local-grid based contact autoencoder.
     """
-    def __init__(self, grid_ae, model, diffusion, cfg):
+    def __init__(self, grid_ae, hand_ae, model, diffusion, cfg):
         super(GraspDiffTrainer, self).__init__(grid_ae=grid_ae, model=model, diffusion=diffusion, cfg=cfg)
+        self.hand_ae = hand_ae
     
     def train_val_step(self, batch, batch_idx, stage):
         self.grid_coords = self.grid_coords.view(-1, 3).to(self.device)
@@ -39,15 +40,17 @@ class GraspDiffTrainer(LGCDiffTrainer):
         lg_contact = rearrange(lg_contact, 'b n k1 k2 k3 c -> (b n) c k1 k2 k3')
         posterior, obj_feat, multi_scale_obj_cond = self.grid_ae.encode(lg_contact, obj_msdf)
         # z = torch.cat([n_ho_dist.unsqueeze(-1), posterior.sample().view(batch_size, n_grids, -1)], dim=-1) # n_dim + 1
-        z = posterior.sample().view(batch_size, n_grids, -1) # n_dim
-        obj_pc = torch.cat([obj_msdf_center, obj_feat.view(batch_size, n_grids, -1), z], dim=-1)
+        contact_latent = posterior.sample().view(batch_size, n_grids, -1) # n_dim
+        obj_pc = torch.cat([obj_msdf_center, obj_feat.view(batch_size, n_grids, -1)], dim=-1)
+        hand_z = self.hand_ae.encode(handobject.hand_verts)
+        hand_latent = hand_z.sample()
 
         ## Pad the shape params with 0
-        betas = torch.zeros(batch_size, 10, device=self.device)
-        mano_param_vec = handobject.get_99_dim_mano_params()  # B x 99
-        mano_param_vec = torch.cat([mano_param_vec, betas], dim=-1) # B x 109
+        # betas = torch.zeros(batch_size, 10, device=self.device)
+        # mano_param_vec = handobject.get_99_dim_mano_params()  # B x 99
+        # mano_param_vec = torch.cat([mano_param_vec, betas], dim=-1) # B x 109
 
-        input_data = {'x': mano_param_vec, 'obj_pc': obj_pc.permute(0, 2, 1)}
+        input_data = {'x': contact_latent, 'y': hand_latent, 'obj_pc': obj_pc.permute(0, 2, 1)}
 
         # Check for NaN in input data
         for key, value in input_data.items():

common/model/handobject.py
@@ -96,6 +96,7 @@ class HandObject:
         self.hand_joints = batch['handJoints'].clone().to(self.device).float()
         self.obj_verts = batch['objSamplePts'].clone().to(self.device).float()
         self.obj_normals = batch['objSampleNormals'].clone().to(self.device).float()
+        self.cano_joints = batch['canoJoints'].clone().to(self.device).float()
         self.batch_size = self.obj_rot.shape[0]
         # handV, handJ = batch['handVerts'].clone().cpu().numpy(), batch['handJoints'][:, :16].clone().cpu().numpy()
         # self.hand_models = [trimesh.Trimesh(handV[i], self.closed_hand_faces.copy()) for i in range(handV.shape[0])]
@@ -140,9 +141,11 @@ class HandObject:
             homo_hand_joints = F.pad(self.hand_joints, (0, 1), 'constant', 1)
             self.hand_joints = (objT_inv.unsqueeze(1) @ homo_hand_joints.unsqueeze(-1))[:, :, :3, 0]
 
+            root_j = self.cano_joints[:, 0]
             ## Transform hand rotation & translation
             self.hand_root_rot = matrix_to_axis_angle(objT_inv[:, :3, :3] @ axis_angle_to_matrix(self.hand_root_rot))
-            self.hand_trans = (objT_inv[:, :3, :3] @ self.hand_trans[:, :, None]).squeeze(-1) + objT_inv[:, :3, 3]
+            self.hand_trans = (objT_inv[:, :3, :3] @ (self.hand_trans + root_j).unsqueeze(-1)).squeeze(-1) + objT[:, :3, 3] - root_j
+            # self.hand_trans = (objT_inv[:, :3, :3] @ self.hand_trans[:, :, None]).squeeze(-1) + objT_inv[:, :3, 3]
 
             # Compute and transform hand normals
             # self.hand_normals = torch.stack([

common/model/vae/handvae.py
@@ -163,7 +163,6 @@ class HandVAETrainer(LightningModule):
         super(HandVAETrainer, self).__init__()
         self.model = model
         self.criterion = nn.L1Loss()
-        self.param_guided_epoch = cfg.train.param_guided_epoch
         self.cfg = cfg
 
     def training_step(self, batch, batch_idx):
@@ -173,21 +172,24 @@ class HandVAETrainer(LightningModule):
         return self.train_val_step(batch, batch_idx, stage='val')
     
     def train_val_step(self, batch, batch_idx, stage):
-        thetas = batch['theta']
-        betas = batch['beta']
-        ## Augment the betas by adding noise.
-        betas = betas + torch.randn_like(betas) * 0.1
-        pose_6d = matrix_to_rotation_6d(axis_angle_to_matrix(thetas[:, 3:].view(-1, 15, 3))).view(-1, 15*6)
-        nhandV_gt, nhandJ_gt, _ = self.model.mano_layer(
-            torch.cat([torch.zeros(thetas.shape[0], 3, device=self.device), thetas[:, 3:]], dim=-1),
-            th_betas=betas)
+        # thetas = batch['theta']
+        # betas = batch['beta']
+        # ## Augment the betas by adding noise.
+        # betas = betas + torch.randn_like(betas) * 0.1
+        # pose_6d = matrix_to_rotation_6d(axis_angle_to_matrix(thetas[:, 3:].view(-1, 15, 3))).view(-1, 15*6)
+        # nhandV_gt, nhandJ_gt, _ = self.model.mano_layer(
+        #     torch.cat([torch.zeros(thetas.shape[0], 3, device=self.device), thetas[:, 3:]], dim=-1),
+        #     th_betas=betas)
 
-        hand_param = torch.cat([pose_6d, betas], dim=-1)
-        # handobject = HandObject(self.cfg.data, self.device, mano_layer=self.model.mano_layer, normalize=False)
-        # handobject.load_from_batch(batch)
-        # hand_param = handobject.get_99_dim_mano_params()  # B x 99
-        # handV_gt = handobject.hand_verts
-        # handJ_gt = handobject.hand_joints
+        handobject = HandObject(self.cfg.data, self.device, mano_layer=self.model.mano_layer, normalize=False)
+        handobject.load_from_batch(batch)
+        handV_gt = handobject.hand_verts
+        handJ_gt = handobject.hand_joints
+        root_j = handobject.cano_joints[:, 0]
+        hand_root_R = axis_angle_to_matrix(handobject.hand_root_rot)
+        hand_trans = handobject.hand_trans
+        nhandV_gt = (handV_gt - root_j.unsqueeze(1) - hand_trans.unsqueeze(1)) @ hand_root_R + root_j.unsqueeze(1)
+        nhandJ_gt = (handJ_gt - root_j.unsqueeze(1) - hand_trans.unsqueeze(1)) @ hand_root_R + root_j.unsqueeze(1)
 
         # Verify that hand_param and handV_gt match
         # if batch_idx == 0:
@@ -205,18 +207,18 @@ class HandVAETrainer(LightningModule):
         #             print(f"[Warning] Hand param and vertices mismatch!")
 
         recon_param, handV_pred, handJ_pred, posterior = self.model(nhandV_gt.permute(0, 2, 1))
-        param_loss = self.criterion(recon_param, hand_param) # Ignore the shape param here.
+        # param_loss = self.criterion(recon_param, hand_param) # Ignore the shape param here.
 
-        w_param = self.cfg.train.loss_weights.w_param 
+        # w_param = self.cfg.train.loss_weights.w_param 
             # print(f"Epoch {self.current_epoch}: Switching to reconstruction + KL loss.")
 
         recon_loss = self.criterion(handV_pred, nhandV_gt) + self.criterion(handJ_pred, nhandJ_gt)
         kld_loss = posterior.kl().mean()
-        loss = w_param * param_loss + self.cfg.train.loss_weights.w_recon * recon_loss + self.cfg.train.loss_weights.w_kl * kld_loss
+        loss = self.cfg.train.loss_weights.w_recon * recon_loss + self.cfg.train.loss_weights.w_kl * kld_loss
 
         loss_dict = {
             f'{stage}/total_loss': loss,
-            f'{stage}/param_loss': param_loss,
+            # f'{stage}/param_loss': param_loss,
             f'{stage}/recon_loss': recon_loss,
             f'{stage}/kld_loss': kld_loss
         }
@@ -242,6 +244,36 @@ class HandVAETrainer(LightningModule):
                     self.logger.experiment.log({f'{stage}/hand_recon': wandb.Image(vis_img)}, step=self.global_step)
 
         return loss
+    
+    def on_test_batch_start(self, batch, batch_idx):
+        self.recon_err_list = []
+
+    def test_step(self, batch, batch_idx):
+        thetas = batch['theta']
+        betas = batch['beta']
+        ## Augment the betas by adding noise.
+        betas = betas + torch.randn_like(betas)
+        pose_6d = matrix_to_rotation_6d(axis_angle_to_matrix(thetas[:, 3:].view(-1, 15, 3))).view(-1, 15*6)
+        nhandV_gt, nhandJ_gt, _ = self.model.mano_layer(
+            torch.cat([torch.zeros(thetas.shape[0], 3, device=self.device), thetas[:, 3:]], dim=-1),
+            th_betas=betas)
+            
+        # handobject = HandObject(self.cfg.data, self.device, mano_layer=self.model.mano_layer, normalize=False)
+        # handobject.load_from_batch(batch)
+        # handV_gt = handobject.hand_verts
+        # handJ_gt = handobject.hand_joints
+        # root_j = handobject.cano_joints[:, 0]
+        # hand_root_R = axis_angle_to_matrix(handobject.hand_root_rot)
+        # hand_trans = handobject.hand_trans
+        # nhandV = (handV_gt - root_j.unsqueeze(1) - hand_trans.unsqueeze(1)) @ hand_root_R + root_j.unsqueeze(1)
+        recon_param, handV_pred, handJ_pred, posterior = self.model(nhandV_gt.permute(0, 2, 1))
+        recon_error = torch.norm(handV_pred - nhandV_gt, dim=-1).mean(dim=-1)  # B
+        self.recon_err_list.append(recon_error.detach().cpu().numpy())
+    
+    def on_test_epoch_end(self):
+        avg_recon_err = np.concatenate(self.recon_err_list, axis=0).mean() * 1000
+        print(f"Average hand vertex reconstruction error: {avg_recon_err:.6f} mm")
+
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.parameters(), lr=self.cfg.train.lr)

scripts/debug_test.py
@@ -6,9 +6,10 @@ from matplotlib import pyplot as plt
 from common.manopth.manopth.manolayer import ManoLayer
 from common.dataset_utils.grab_dataset import GRABDataset
 from common.dataset_utils.datamodules import HOIDatasetModule, LocalGridDataModule
-from common.utils.vis import visualize_local_grid, visualize_local_grid_with_hand
+from common.utils.vis import visualize_local_grid, visualize_local_grid_with_hand, o3dmesh
 from common.msdf.utils.msdf import get_grid
 from common.dataset_utils.hoi4d_dataset import HOI4DHandDataModule
+from pytorch3d.transforms import axis_angle_to_matrix, matrix_to_axis_angle
 # from common.model.vae.grid_vae import MLCVAE
 from tqdm import tqdm
 import numpy as np
@@ -229,6 +230,30 @@ def compare_ckpt():
     print(ckpt2)
 
 
+def test_manolayer():
+    mano_layer = ManoLayer(mano_root = 'data/misc/mano_v1_2/models', use_pca=False, side='right', flat_hand_mean=True, ncomps=45)
+    thetas = torch.randn(1, 48)
+    betas = torch.randn(1, 10)
+    trans = torch.randn(1, 3)
+    faces = mano_layer.th_faces.detach().cpu().numpy()
+    _, cano_joints, _ = mano_layer(torch.zeros_like(thetas), th_betas=betas)
+    verts, joints, _ = mano_layer(thetas, th_betas=betas, th_trans=trans)
+    # root_j = joints[:, 0]
+    targetR = axis_angle_to_matrix(torch.randn(1, 3))
+    targett = torch.randn(1, 3)
+    verts = verts @ targetR.transpose(-1, -2) + targett.unsqueeze(1)
+    root_rot = axis_angle_to_matrix(thetas[:, :3])
+    new_root_rot = matrix_to_axis_angle(targetR @ root_rot)
+    thetas[:, :3] = new_root_rot
+    new_trans = (targetR @ trans.unsqueeze(-1) + targetR @ root_j.unsqueeze(-1)).squeeze(-1) + targett - root_j
+    new_verts, new_joints, _ = mano_layer(thetas, th_betas=betas, th_trans=new_trans)
+
+    ori_mesh = o3dmesh(verts[0].cpu().numpy(), faces, color=[0, 0, 1]) # blue
+    new_mesh = o3dmesh(new_verts[0].cpu().numpy(), faces, color=[1, 0, 0]) # red
+    coord_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1)
+    o3d.visualization.draw_geometries([ori_mesh, new_mesh, coord_frame])
+
+
 # Usage Example
 def test_hoi4d_datamodule():
     from omegaconf import OmegaConf
@@ -279,4 +304,5 @@ if __name__ == "__main__":
     # vis_local_grid_interact()
     # test_pointvae()
     # compare_ckpt()
-    test_hoi4d_datamodule()
+    # test_hoi4d_datamodule()
+    test_manolayer()